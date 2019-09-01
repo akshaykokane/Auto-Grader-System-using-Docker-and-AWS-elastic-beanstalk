caa2db7edda9861a703bc578d6a4a0c3261e8e20
Akshay Kokane <30575487+akshaykokane@users.noreply.github.com>
2019-09-01 10:57:56 -0400

    -completed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-import random
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+from exe import autoGrade
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-   return "file uploaded successfully. AUTOGRADING IN PROGRESS.."
+   return autoGrade();
     
 if __name__ == "__main__":
     app.debug = True
