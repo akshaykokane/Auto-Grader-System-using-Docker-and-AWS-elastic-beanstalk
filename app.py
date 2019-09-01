dce86301db5b2d717da004c5f2deb4bfe1a5b16f
Akshay Kokane <30575487+akshaykokane@users.noreply.github.com>
2019-09-01 10:28:08 -0400

    error-fixes

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = random.choice(images)
     return render_template('index.html', url=url)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
-      f.save(secure_filename(f.filename)) 
+      f.save(secure_filename(f.filename))
    return "file uploaded successfully. AUTOGRADING IN PROGRESS.."
     
 if __name__ == "__main__":
+    app.debug = True
     app.run(host="0.0.0.0")
